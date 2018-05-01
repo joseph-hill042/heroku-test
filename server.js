4a3e5df86d1eb1d3d2060b5fe1de1c9ed8f19bdb
Joseph Hill <joseph.hill1@outlook.com>
2018-04-30 20:17:55 -0400

    Added procfile and server config

---
+++ server.js
@@ -0,0 +1,14 @@
+// server
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 5000);
+
+// Our first route
+app.get('/', function (req, res) {
+    res.send('Hello Node + GitHub!');
+});
+
+// Listen to port
+app.listen(app.get('port'), function () {
+    console.log('App is listening on port ' + app.get('port'));
+});
